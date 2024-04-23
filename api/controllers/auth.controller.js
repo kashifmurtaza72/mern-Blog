9f8a709ec0f924b8694b6736f830d5e6d32e4cdd
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2024-04-23 06:55:40 +0500

    signup api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,26 @@
-export const signup = (req, res) => {
-   console.log(req.body)
-}
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
 
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({ username, email, password: hashedPassword }); //actualy it should be {username: username} but after ES6 if the key and value is similar we can just remove one
+
+  try {
+    await newUser.save();
+    res.json("Signup successful");
+  } catch (error) {
+     res.status(500).json({ message: error.message });
+  }
+};
