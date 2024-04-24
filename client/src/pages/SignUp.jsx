08e18601da16b11163ed6fefdc89f1c392253e1b
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2024-04-24 06:26:52 +0500

    adding functionality to signup page ..

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,42 +1,83 @@
 import { Button, Label, TextInput } from "flowbite-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const handleChange = (event) => {
+    //console.log(event.target.value);
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+    } catch (event) {
+      console.log('kashif');
+    }
+    //console.log(formData);
+  };
+  //console.log(formData);
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* left */}
         <div className="flex-1">
-          <Link
-            to='/'
-            className='font-bold dark:text-white text-4xl'
-          >
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              Sahand's
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Sahands
             </span>
             Blog
           </Link>
           <p className="text-sm mt-5">
-            This is a demo project. You can sign up with your email and password or with Google
+            This is a demo project. You can sign up with your email and password
+            or with Google
           </p>
-
         </div>
         {/* right */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Your Username" />
-              <TextInput type="text" placeholder="username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="username"
+                id="username"
+                name="username"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Your Email" />
-              <TextInput type="email" placeholder="name@company.com" id="email" />
+              <TextInput
+                type="email"
+                name="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Your Password" />
-              <TextInput type="password" placeholder="password" id="password" />
+              <TextInput
+                type="password"
+                name="password"
+                placeholder="password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone='purpleToPink' type="submit">Sign Up </Button>
+            <Button gradientDuoTone="purpleToPink" type="submit">
+              Sign Up{" "}
+            </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
@@ -45,9 +86,7 @@ export default function SignUp() {
             </Link>
           </div>
         </div>
-
       </div>
-
     </div>
-  )
+  );
 }
