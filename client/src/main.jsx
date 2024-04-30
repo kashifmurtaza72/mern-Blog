db67027e7462bcc250c77ed87b68ccbb918683e7
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2024-04-30 16:01:47 +0500

    Add redux Toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,15 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { store } from './redux/store.js';
+import { Provider } from 'react-redux'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+
