0b6f8f34545555031583e2cedaa26af931e7740a
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2024-04-30 07:04:57 +0500

    complete signin page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -49,7 +49,6 @@ export const signin = async (req, res, next) => {
     res.status(200).cookie('access_token', token, { httpOnly: true }).json(rest);
 
 
-
   } catch (error) {
     next(error);
   }
