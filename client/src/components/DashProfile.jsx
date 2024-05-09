5a2b6b9ac4db16985002aab773c8e890b763e147
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2024-05-09 16:56:34 +0500

    Complete User Image Upload Functionalitypart 1

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,15 +1,51 @@
 import { useSelector } from "react-redux";
 import { Button, TextInput } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
+import {getStorage} from "firebase/storage";
+import { app } from "../firebase";
+
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  //console.log(imageFile, imageFileUrl)
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+  const uploadImage = async () => {
+    //    /databases/(default)/documents/users/$(request.auth.uid)).data.isAdmin;
+    // service firebase.storage {
+    //   match /b/{bucket}/o {
+    //     match /{allPaths=**} {
+    //       allow read;
+    //       allow write: if
+    //       request.resource.size < 2 * 1024 * 1024 && 
+    //       request.resource.contentType.matches('image/.*')
+    //     }
+    //   }
+    // }
+    //console.log('uploading image...')
+    const storage = getStorage(app);
+  };
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
 
       <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+        <input type="file" hidden accept="image/*" onChange={handleImageChange} ref={filePickerRef} />
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() => { filePickerRef.current.click() }}>
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user"
             className="rounded-full w-full h-full border-8 border-[lightgray] object-cover"
           />
