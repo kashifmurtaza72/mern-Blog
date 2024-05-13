babc9b221dde19a2d3fd5b6ce803972e00e9bd51
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2024-05-13 16:56:04 +0500

    Add update user API route complete

--- api/utils/verifiyUser.js
+++ api/utils/verifiyUser.js
@@ -3,7 +3,7 @@ import { errorHandler } from './error.js';
 
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
-    console.log(req)
+    //console.log(req)
     if (!token) {
         return next(errorHandler(401, 'Unauthorized'));
     }
