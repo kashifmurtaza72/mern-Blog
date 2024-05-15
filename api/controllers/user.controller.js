65a2cc31bb66e00f30fdc2fd93e3c7d65d78c056
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2024-05-15 23:53:53 +0500

    Add delete user API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -56,3 +56,17 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  //console.log(req);
+  //console.log(req.user.id, req.params.userId)
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this User."));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json({ message: "User has been deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { test, updateUser } from '../controllers/user.controller.js'; //remember to put.js at end
+import { deleteUser, test, updateUser } from '../controllers/user.controller.js'; //remember to put.js at end
 import { verifyToken } from '../utils/verifiyUser.js';
 const router = express.Router();
 
 router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
+
 
 export default router; // as we are exporting default we can change its name, here we say router but in index.js we can say userRoutes
