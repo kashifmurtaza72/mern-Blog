eb951ed75c56aa4a1f44eff2d58359ce8bf60f9a
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2024-06-12 00:59:01 +0500

    create get posts API route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -28,23 +28,23 @@ export const create = async (req, res, next) => {
 
 export const getposts = async (req, res, next) => {
     try {
+        //console.log(req);
         const startIndex = parseInt(req.query.startIndex) || 0
         const limit = parseInt(req.query.limit) || 9
         const sortDirection = req.query.order === 'asc' ? 1 : -1
 
-        const posts = await Post.find({
+        const posts = await Post.find({ 
             ...(req.query.userId && { userId: req.query.userId }),
             ...(req.query.category && { category: req.query.category }),
             ...(req.query.slug && { slug: req.query.slug }),
-            ...(req.query.postId && { postId: req.query.postId }),
-            ...(req.query.userId && { _id: req.query.userId }),
+            ...(req.query.postId && { _id: req.query.postId }),
             ...(req.query.searchTerm && {
                 $or: [
                     { title: { $regex: req.query.searchTerm, $options: 'i' } },
                     { content: { $regex: req.query.searchTerm, $options: 'i' } },
-                ].sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit)
+                ],
             }),
-        });
+        }).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit)
 
         const totalPosts = await Post.countDocuments()
 
@@ -52,10 +52,9 @@ export const getposts = async (req, res, next) => {
 
         const oneMonthAgo = new Date(
             now.getFullYear(), 
-            now.getFullMonth()-1,
+            now.getMonth()-1,
             now.getDate()
         );
-
         const lastMonthPosts = await Post.countDocuments({
             createdAt : { $gte : oneMonthAgo }, 
         });
