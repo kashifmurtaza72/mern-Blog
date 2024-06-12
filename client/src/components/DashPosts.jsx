7afda171fd581b3d5bdad7e43dac232d02539efa
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2024-06-12 11:13:57 +0500

    Show user posts inside dashboard2

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
-  console.log(userPosts);
+  //console.log(userPosts);
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -25,7 +25,7 @@ export default function DashPosts() {
     }
   }, [currentUser._id]);
   return (
-    <div>
+    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && userPosts.length > 0 ? (
         <>
           <Table hoverable className="shadow-md">
@@ -40,25 +40,40 @@ export default function DashPosts() {
               </Table.HeadCell>
             </Table.Head>
             {userPosts.map((post) => (
-              <Table.Body>
-                <Table.Row>
+              <Table.Body className="divide-y">
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                   <Table.Cell>
                     {new Date(post.updatedAt).toLocaleDateString()}
                   </Table.Cell>
 
                   <Table.Cell>
-                   <Link to={`/post/${post.slug}`}>
-                   <img src={post.image} alt={post.title}  className="w-20 h-10 object-cover"/></Link>
+                    <Link to={`/post/${post.slug}`}>
+                      <img src={post.image} alt={post.title} className="w-20 h-10 object-cover" /></Link>
                   </Table.Cell>
 
                   <Table.Cell>
-                   <Link to={`/post/${post.slug}`}>
-                   {post.title}</Link>
+                    <Link to={`/post/${post.slug}`} className="font-medium text-gray-950 dark:text-white">
+                      {post.title}</Link>
                   </Table.Cell>
+
+                  <Table.Cell>
+                    {post.category}
+                  </Table.Cell>
+
+                  <Table.Cell>
+                    <Link to={`/delete-post/${post._id}`} className="font-medium text-red-500 hover:underline cursor-pointer">
+                      <span>Delete</span>
+                    </Link>
+                  </Table.Cell>
+
+                  <Table.Cell>
+                    <Link to={`/update-post/${post._id}`} className="text-teal-500">
+                      <span>Edit</span>
+                    </Link>
+                  </Table.Cell>
+
                 </Table.Row>
 
-
-                <Table.Row></Table.Row>
               </Table.Body>
             ))}
           </Table>
