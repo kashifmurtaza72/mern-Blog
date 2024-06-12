8c2b5790579b4d9651103e7a20fe0463d1bc903a
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2024-06-12 08:23:17 +0500

    Show user posts inside dashboard 1

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import { Table } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from 'react-router-dom';
 
 export default function DashPosts() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+  console.log(userPosts);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        //console.log(data);
+        if (res.ok) {
+          setUserPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
   return (
-    <div>DashPosts</div>
-  )
+    <div>
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table hoverable className="shadow-md">
+            <Table.Head>
+              <Table.HeadCell>Date Updated</Table.HeadCell>
+              <Table.HeadCell>Post image</Table.HeadCell>
+              <Table.HeadCell>Post title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            {userPosts.map((post) => (
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+
+                  <Table.Cell>
+                   <Link to={`/post/${post.slug}`}>
+                   <img src={post.image} alt={post.title}  className="w-20 h-10 object-cover"/></Link>
+                  </Table.Cell>
+
+                  <Table.Cell>
+                   <Link to={`/post/${post.slug}`}>
+                   {post.title}</Link>
+                  </Table.Cell>
+                </Table.Row>
+
+
+                <Table.Row></Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+        </>
+      ) : (
+        <p>There is no post yet</p>
+      )}
+    </div>
+  );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -35,5 +35,6 @@ module.exports = {
   plugins: [
     // ...
     flowbite.plugin(),
+    require('tailwind-scrollbar')
   ],
 };
