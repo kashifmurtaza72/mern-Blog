4953cc5a123ee52453b6a9c2596fe10ea6a15122
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2024-06-27 07:40:45 +0500

    create get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, signout, test, updateUser } from '../controllers/user.controller.js'; //remember to put.js at end
+import { deleteUser, getUsers, signout, test, updateUser } from '../controllers/user.controller.js'; //remember to put.js at end
 import { verifyToken } from '../utils/verifiyUser.js';
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
+router.get('/getusers', verifyToken, getUsers);
 
 
 export default router; // as we are exporting default we can change its name, here we say router but in index.js we can say userRoutes
