d518b08e74f9d1d781ccf1127c098b1d3507b277
kashifmurtaza72 <70048358+kashifmurtaza72@users.noreply.github.com>
2024-07-25 17:00:44 +0500

    comment-1

---
+++ client/src/components/CommentSection.jsx
@@ -0,0 +1,41 @@
+import { Button, Textarea } from 'flowbite-react'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+export default function CommentSection({ postId }) {
+    const { currentUser } = useSelector(state => state.user)
+    const [comment, setComment] = useState(null)
+    const handleSubmit = async (e) => {
+        
+    }
+    return (
+        <div className='max-w-2xl mx-auto w-full p-3'>
+            {currentUser ? (
+                <div className="flex items-center gap-1 my-5 text-gray-500 text-sm">
+                    <p>Signed in as:</p>
+                    <img className='w-5 h-5 object-cover rounded-full' src={currentUser.profilePicture} alt='' />
+                    <Link to={'/dashboard?tab=profile'} className='text-xs text-cyan-600 hover:underline'>@{currentUser.username}</Link>
+                </div>
+            ) : (
+                <div className="text-sm text-teal-500 my-5 flex gap-1">
+                    You must be login to comment.
+                    <Link className='text-blue-500 hover:underline' to='/signin'>Login</Link>
+                </div>
+            )}
+
+            {currentUser && (
+                <form onSubmit={handleSubmit} className='border border-teal-500 rounded-md p-3'>
+                    <Textarea
+                        placeholder='Add a comment...'
+                        rows='3'
+                        maxLength='200' onChange={(e)=>setComment(e.target.value)} value={comment}
+                    />
+                    <div className="flex justify-between items-center mt-5">
+                        <p className='text-gray-500 text-xs'>{200 - comment.length} Characters remaining</p>
+                        <Button gradientDuoTone='purpleToBlue' outline type='submit'>Submit</Button>
+                    </div>
+                </form>
+            )}
+        </div>
+    )
+}
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
+import CommentSection from "../components/CommentSection";
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
@@ -65,6 +66,7 @@ export default function PostPage() {
 
       <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{ __html: post && post.content }}></div>
       <div className="max-w-4xl mx-auto w-full"><CallToAction /></div>
+      <CommentSection postId={post._id}/>
     </main>
   );
 }
