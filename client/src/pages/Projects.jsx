20396eb2ef09622ac681f9a9f1f40e9eab736fd2
kashifmurtaza72 <70048358+kashifmurtaza72@users.noreply.github.com>
2024-09-01 15:35:42 +0500

    about and project page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,13 @@
+import CallToAction from "../components/CallToAction";
 export default function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="min-h-screen max-w-2xl mx-auto flex flex-col justify-center items-center gap-6 ">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-gray-500">
+        Build fun and engaging projects while learning advance features in
+        React, NodeJS, Mongoose and Express.
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
